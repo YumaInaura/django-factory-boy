ff495c50592ffd80bd005afc09a8f1386600c97d
YumaInaura <yuma.inaura@gmail.com>
2019-05-19 14:10:14 +0900

    Factory boy factires directory

--- bar/tests.py
+++ bar/tests.py
@@ -2,10 +2,14 @@ from django.test import TestCase
 
 from bar.factories import BookFactory
 from bar.models import Book
+from faker import Faker
 
 class TestBook(TestCase):
   def test_create_book(self):
-    BookFactory.create()
-    BookFactory.create()
-    self.assertEqual(Book.objects.count(),2)
+    title = Faker().name()
+
+    BookFactory.create(title=title)
+
+    self.assertEqual(Book.objects[0].title,title)
+    self.assertEqual(Book.objects.count(),1)
   
